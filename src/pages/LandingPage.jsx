004e3a760eadf0cd3b4081a75e20310174b1dca5
Utkarsh <utkarshsingx@users.noreply.github.com>
2025-08-24 10:26:51 +0530

    patch

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name.trim().toLowerCase() === 'mahak050703' || name.trim().toLowerCase() === 'miku') {
+    if (name.trim().toLowerCase() === 'mahak' || name.trim().toLowerCase() === 'miku') {
       navigate('/page1');
     } else {
       alert('Access denied. Only certain soul can enter.');
